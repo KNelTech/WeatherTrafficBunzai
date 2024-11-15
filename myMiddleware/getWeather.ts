ef6e6c15088f1d3ceda150091f9643d811f9d9c8
footgun dev <kneldev@gmail.com>
2024-11-14 21:33:33 -0600

    this api is better

--- myMiddleware/getWeather.ts
+++ myMiddleware/getWeather.ts
@@ -1,127 +1,75 @@
-//https://www.weatherapi.com/my/
+//https://home.openweathermap.org/
 
-const API_KEY = Bun.env.API_TOKEN;
+const API_TOKEN = Bun.env.API_TOKEN;
 
+interface Location {
+  name: string;
+  lat: number;
+  lon: number;
+}
 
+interface CleanedForecastItem {
+  dt: number;
+  temp: number;
+  feels_like: number;
+  description: string;
+  wind_speed: number;
+  precipitation_chance: number;
+  main_condition: string;
+  rain?: number;
+  snow?: number;
+}
 
-This api is fuckin dogwater. I wouldnt even slap your mom with It. fuck this tomorrow were doing weatherstack
+const locations: Location[] = [
+  { name: "Diana's work", lat: 41.982330, lon: -88.035810 },
+  { name: "Kodi's work", lat: 41.887920, lon: -87.665649 },
+  { name: "Home", lat: 41.924940, lon: -87.776010 }
+];
 
+function convertUnixToCSTTime(unixTime: number): string {
+  const cstDate = new Date(unixTime * 1000);
+  const cstFormatter = new Intl.DateTimeFormat('en-US', {
+    timeZone: 'America/Chicago',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  });
+  return cstFormatter.format(cstDate);
+}
 
-// async function fetchWeather(zipCode: string) {
-//     const url = `https://api.weatherapi.com/v1/forecast.json?q=${zipCode}&days=7&alerts=no&aqi=no&key=${API_KEY}`;
-
-//     try {
-//         const response = await fetch(url, {
-//             method: 'GET',
-//             headers: {
-//                 'accept': 'application/json'
-//             }
-//         });
-
-//         if (!response.ok) {
-//             const errorText = await response.text();
-//             throw new Error(`HTTP error! status: ${response.status}, message: ${errorText}`);
-//         }
-
-//         return response.json();
-//     } catch (error) {
-//         console.error('Error fetching weather data:', error);
-//         throw error;
-//     }
-// }
-
-// export async function getWeatherForDisplay() {
-//     const zipCodes = ['60639', '60143', '60607'];
-
-//     try {
-//         const weatherPromises = zipCodes.map(zipCode => fetchWeather(zipCode));
-//         const weatherResults = await Promise.all(weatherPromises);
-
-//         return weatherResults;
-//     } catch (error) {
-//         console.error('Error fetching weather data:', error);
-//         throw error;
-//     }
-// }
-
-
-
-
-// async function fetchWeather(zipCode: string) {
-//     const url = `https://api.weatherapi.com/v1/current.json?q=${zipCode}&key=${API_KEY}`;
-    
-//     const response = await fetch(url, {
-//       method: 'GET',
-//       headers: {
-//         'accept': 'application/json'
-//       }
-//     });
+async function fetchWeatherForecast(location: Location) {
+  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${location.lat}&lon=${location.lon}&cnt=8&units=imperial&appid=${API_TOKEN}&units=metric`;
   
-//     if (!response.ok) {
-//       throw new Error(`HTTP error! status: ${response.status}`);
-//     }
-  
-//     return response.json();
-//   }
-  
-//   function cleanWeatherData(data: any) {
-//     return {
-//       location: data.location.name,
-//       temperature: data.current.temp_c,
-//       condition: data.current.condition.text,
-//       icon: data.current.condition.icon
-//     };
-//   }
-  
-//   export async function getWeatherForDisplay() {
-//     const zipCodes = ['60639', '60143', '60607']; 
-  
-//     try {
-//       const weatherPromises = zipCodes.map(zipCode => fetchWeather(zipCode));
-//       const weatherResults = await Promise.all(weatherPromises);
-  
-//       return weatherResults.map(cleanWeatherData);
-//     } catch (error) {
-//       console.error('Error fetching weather data:', error);
-//       throw error;
-//     }
-//   }
+  const response = await fetch(url);
 
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
 
-  // front? I guess? I'm TIRED
+  const data = await response.json();
 
-//   async function loadDisplayWeatherData() {
-//     const response = await fetch('/api/display-weather');
-//     if (!response.ok) {
-//       throw new Error('Failed to fetch display weather data');
-//     }
-//     return response.json();
-//   }
-  
-//   // Usage
-//   loadDisplayWeatherData()
-//     .then(weatherData => {
-//       console.log(weatherData);
-//       // weatherData will be an array of objects like:
-//       // [
-//       //   {
-//       //     location: 'Chicago',
-//       //     current: {
-//       //       temperature: 22,
-//       //       condition: 'Partly cloudy',
-//       //       icon: '//cdn.weatherapi.com/weather/64x64/day/116.png'
-//       //     },
-//       //     forecast: [
-//       //       { date: '2023-12-11', maxTemp: 24, minTemp: 18, condition: 'Sunny', icon: '...' },
-//       //       { date: '2023-12-12', maxTemp: 23, minTemp: 17, condition: 'Cloudy', icon: '...' },
-//       //       // ... (7 days total)
-//       //     ]
-//       //   },
-//       //   // ... (data for other zip codes)
-//       // ]
-//       // Update your UI with this weather data
-//     })
-//     .catch(error => {
-//       console.error('Error loading display weather data:', error);
-//       // Handle the error in your UI
-//     });
+  const cleanedData: CleanedForecastItem[] = data.list.map((item: any) => ({
+    cst_time: convertUnixToCSTTime(item.dt),
+    temp: item.main.temp,
+    feels_like: item.main.feels_like,
+    main_condition: item.weather[0].main,
+    description: item.weather[0].description,
+    wind_speed: item.wind.speed,
+    precipitation_chance: item.pop,
+    rain: item.rain?.['3h'],
+    snow: item.snow?.['3h']
+  }));
+
+  return { [location.name]: cleanedData };
+}
+
+export async function getWeatherForDisplay() {
+  try {
+    const weatherPromises = locations.map(location => fetchWeatherForecast(location));
+    const weatherResults = await Promise.all(weatherPromises);
+    return Object.assign({}, ...weatherResults);
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+    throw error;
+  }
+}
